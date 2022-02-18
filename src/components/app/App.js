b932a9fa8ae5a270badf212067cf6888b5dce4cf
Romanchik Aleksey <romanchikaleksey@example.com>
2022-02-18 10:04:03 +0300

    Add 404 error page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,7 @@
 import AppHeader from "../appHeader/AppHeader";
-import MainPage from "../pages/MainPage";
-import ComicsPage from "../pages/ComicsPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { MainPage, ComicsPage, Page404 } from '../pages';
+
 
 
 const App = () => {
@@ -13,6 +13,7 @@ const App = () => {
                     <Routes>
                         <Route path="/" element={<MainPage />} />
                         <Route path="/comics" element={<ComicsPage />} />
+                        <Route path="*" element={<Page404 />} />
                     </Routes>
                 </main>
             </div>
---
+++ src/components/pages/index.js
@@ -0,0 +1,5 @@
+import MainPage from "./MainPage";
+import ComicsPage from "./ComicsPage";
+import Page404 from './404';
+
+export { MainPage, ComicsPage, Page404 };
